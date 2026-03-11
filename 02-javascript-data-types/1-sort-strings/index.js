488b6d14c51effeb172b6b196963b70cf07bcda4
Bodoni <bodoni.jovan@icloud.com>
2026-03-11 18:54:34 +0100

    Sort array of strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let rus = arr.filter( s => /^[а-яёА-ЯЁ]/.test(s) );
+    let lat = arr.filter( s => /^[a-zA-Z]/.test(s) );
 
+    let res1 = (param == 'asc') ? rus.sort( (a, b) => a.localeCompare(b, 'ru', {caseFirst: "upper"}) ) : (param == 'desc') ? rus.reverse().sort( (a, b) => b.localeCompare(a, 'ru', {sensitivity: "variant"}) ) : arr;
+
+    let res2 = (param == 'asc') ? lat.sort( (a, b) => a.localeCompare(b, 'en', {caseFirst: "upper"}) ) : (param == 'desc') ? lat.reverse().sort( (a, b) => b.localeCompare(a, 'en', {sensitivity: "variant"}) ) : arr;
+
+    return param === 'asc' ? [...res1, ...res2] : [...res2, ...res1];
 }
