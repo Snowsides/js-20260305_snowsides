0bda1515e997daf67ec87e186081a5dab36cbf96
Bodoni <bodoni.jovan@icloud.com>
2026-03-12 09:32:19 +0100

    Creates an object composed of the picked object properties

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,21 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
-
+const fruits = {
+ apple: 2,
+ orange: 4,
+ banana: 3
 };
+
+export const pick = (obj, ...fields) => {
+    
+    let res =  Object.fromEntries(
+        Object.entries(obj).filter( ([key]) => fields.includes(key) )
+    );
+
+    return res;
+};
+
+
+console.log(pick(fruits, 'apple', 'banana')); // { apple: 2, banana: 3 }
+
