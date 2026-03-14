ff75a574631067c7737b7e704a906fca8995437c
Bodoni <bodoni.jovan@icloud.com>
2026-03-14 14:40:25 +0100

    invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj === undefined) return undefined;
+    if (Object.keys(obj).length === 0) return {};
+    const res = Object.entries(obj).reduce( (acc, [key, value]) => {
+        acc[value] = key;
+        return acc;
+    }, {} );
+    return res;
 }
