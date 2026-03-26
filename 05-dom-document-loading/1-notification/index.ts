ccdc3e0ebb6e1cfc8497534a4cbb806058d2d613
Bodoni <bodoni.jovan@icloud.com>
2026-03-26 10:41:09 +0100

    05-dom-document fix3

--- 05-dom-document-loading/1-notification/index.ts
+++ 05-dom-document-loading/1-notification/index.ts
@@ -2,37 +2,40 @@ import { createElement } from "../../shared/utils/create-element";
 
 interface Options {
   target?: HTMLElement;
-  message: string;
-  options: {
-    duration: number;
-    type: string;
+  message?: string;
+  options?: {
+    duration?: number;
+    type?: string;
   };
 }
 
 export default class NotificationMessage {
   static activeNotification: NotificationMessage | null = null;
   public element: HTMLElement | undefined;
-  public target: HTMLElement | undefined;
   public message: string;
-  private options: { duration?: number; type?: string } = { duration: 0, type: 'success' };
+  public timerId: ReturnType<typeof setTimeout> | undefined;
+  private options: { duration: number; type: string } = { duration: 0, type: 'success' };
 
-  constructor( message: string, options: { duration?: number; type?: string } = {} ) {
+  constructor({ target, message = "", options }: Options = {}) {
     this.message = message;
-    this.options = { duration: options.duration || 0, type: options.type || 'success' };
+    this.target = target;
+    this.timerId = undefined;
+    this.options = {
+      duration: options?.duration || 0,
+      type: options?.type || "success",
+    };
+
     if (NotificationMessage.activeNotification) {
       NotificationMessage.activeNotification.remove();
     }
     NotificationMessage.activeNotification = this;
-    this.show();
   }
 
-  show( target?: HTMLElement ) {
-    this.target = target;
-
+  show() {
     if( this.element && document.body.contains(this.element) ) {
       this.element.remove();
     }
-
+ 
     this.element = createElement(`
       <div class="notification ${this.options?.type ? `${this.options.type}` : 'error'}" style="--value:${this.options?.duration ? this.options.duration / 1000 : 0}s">
         <div class="timer"></div>
@@ -46,17 +49,13 @@ export default class NotificationMessage {
         </div>
       </div>
     `);
-    
+ 
     if (this.element) {
-      if(target instanceof HTMLElement) {
-        target.append(this.element);
-      } else {
-        document.body.append(this.element);
-      }
+      document.body.append(this.element);
     }
-  
-    if (this.options?.duration) {
-      setTimeout( () => {
+ 
+    if  (this.options?.duration  ) {
+      this.timerId = setTimeout( () => {
         this.remove();
       }, this.options.duration);
     }
@@ -64,14 +63,15 @@ export default class NotificationMessage {
   }
 
   remove() {
-    if (this.element && document.body.contains(this.element)) {
-      this.element.remove();
-    }
-  }
-  
-  destroy() {    
-    this.remove();
+  clearTimeout(this.timerId);
+  this.timerId = undefined;
+  if (this.element && document.body.contains(this.element)) {
+    this.element.remove();
     this.element = undefined;
+  }
+}
+  destroy() {
+    this.remove();
     if (NotificationMessage.activeNotification === this) {
       NotificationMessage.activeNotification = null;
     }
