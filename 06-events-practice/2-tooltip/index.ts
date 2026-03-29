fc4069c09cd67d33f2efd263d051b4e966b47d91
Bodoni <bodoni.jovan@icloud.com>
2026-03-29 21:26:46 +0200

    Tooltip fix

--- 06-events-practice/2-tooltip/index.ts
+++ 06-events-practice/2-tooltip/index.ts
@@ -13,20 +13,18 @@ export default class Tooltip {
     document.addEventListener('pointerout', this.onPointerOut);
   }
 
-  render(text: string, pos1: string, pos2: string) {
+  render(text: string) {
     this.element = document.createElement('div');
     this.element.className = 'tooltip';
     this.element.textContent = text;
-    this.element.style.cssText = `position: fixed; left: ${pos1}; top: ${pos2}`;
     document.body.append(this.element);
   }
 
   onPointerOver = (event: PointerEvent) => {
     const target = (event.target as HTMLElement).closest<HTMLElement>('[data-tooltip]');
     if (!target) return;
-    let pos1 = `${event.clientX + 10}px`;
-    let pos2 = `${event.clientY + 10}px`;
-    this.render(target.dataset.tooltip || '', pos1, pos2);
+    this.render(target.dataset.tooltip || '');
+    this.element!.style.cssText = `position: fixed; left: ${event.clientX + 10}px; top: ${event.clientY + 10}px;`;
   }
 
   onPointerOut = (event: PointerEvent) => {
